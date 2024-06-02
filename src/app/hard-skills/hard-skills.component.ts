e77088fde784b00072ccf954e1aaa3ddd8f4cb85
Raffael Eloi <raffaeleloi121@gmail.com>
2024-06-02 13:56:21 -0300

    create backend section with title (GREEN)

--- src/app/hard-skills/hard-skills.component.ts
+++ src/app/hard-skills/hard-skills.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {CdkDragDrop, moveItemInArray, CdkDrag, CdkDropList} from '@angular/cdk/drag-drop';
 import {MatChipsModule} from '@angular/material/chips';
 import { hardSkills } from './datasource/hard-skills';
+import { HardSkillCategory } from './enums/hard-skill-category';
 
 @Component({
   selector: 'app-hard-skills',
@@ -11,7 +12,7 @@ import { hardSkills } from './datasource/hard-skills';
   styleUrl: './hard-skills.component.css'
 })
 export class HardSkillsComponent {
-  hardSkills = hardSkills;
+  backendSkills = hardSkills.filter(hardSkill => hardSkill.category == HardSkillCategory.Backend);
   
   drop(event: CdkDragDrop<string[]>) {
     moveItemInArray(hardSkills, event.previousIndex, event.currentIndex);
