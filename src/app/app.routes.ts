aed30f9b60ea3c4e1dbebe8514e5c84dfb31ad08
Raffael Eloi <raffaeleloi121@gmail.com>
2024-06-02 13:20:42 -0300

    add initial style to header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,6 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 
 export const routes: Routes = [
-    {path: '', component: AppComponent},
+    {path: '', component: HeaderComponent},
     {path: 'header', component: HeaderComponent},
 ];
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule, MatSidenavModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
