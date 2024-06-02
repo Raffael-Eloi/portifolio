2bb3a68a14d5d8d3491e2e89a1592d47a18a5c50
Raffael Eloi <raffaeleloi121@gmail.com>
2024-06-02 17:28:55 -0300

    add education component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HardSkillsComponent } from './hard-skills/hard-skills.component';
 import { WorkExperiencesComponent } from './work-experiences/work-experiences.component';
+import { EducationComponent } from './education/education.component';
 
 export const routes: Routes = [
     {path: '', component: HeaderComponent},
     {path: 'header', component: HeaderComponent},
     {path: 'hard-skills', component: HardSkillsComponent},
     {path: 'work-expeciences', component: WorkExperiencesComponent},
+    {path: 'education', component: EducationComponent},
 ];
