5596664e5abb82f0e26a80363c8c6f8554c0c6fe
Raffael Eloi <raffaeleloi121@gmail.com>
2024-06-02 14:39:22 -0300

    create database section (GREEN)

--- src/app/hard-skills/hard-skills.component.ts
+++ src/app/hard-skills/hard-skills.component.ts
@@ -3,11 +3,12 @@ import {CdkDragDrop, moveItemInArray, CdkDrag, CdkDropList} from '@angular/cdk/d
 import {MatChipsModule} from '@angular/material/chips';
 import { hardSkills } from './datasource/hard-skills';
 import { HardSkillCategory } from './enums/hard-skill-category';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-hard-skills',
   standalone: true,
-  imports: [MatChipsModule, CdkDropList, CdkDrag],
+  imports: [MatChipsModule, CdkDropList, CdkDrag, NgFor],
   templateUrl: './hard-skills.component.html',
   styleUrl: './hard-skills.component.css'
 })
@@ -15,6 +16,7 @@ export class HardSkillsComponent {
   backendSkills = hardSkills.filter(hardSkill => hardSkill.category == HardSkillCategory.Backend);
   frontendSkills = hardSkills.filter(hardSkill => hardSkill.category == HardSkillCategory.Frontend);
   testingSkills = hardSkills.filter(hardSkill => hardSkill.category == HardSkillCategory.Testing);
+  databaseSkills = hardSkills.filter(hardSkill => hardSkill.category == HardSkillCategory.Database);
   
   drop(event: CdkDragDrop<string[]>) {
     moveItemInArray(hardSkills, event.previousIndex, event.currentIndex);
